e3b4c421cb28f99d6d8707fa4bc45e01066fb295
LoneRifle <LoneRifle@users.noreply.github.com>
2019-03-28 11:54:05 +0800

    Provide an index.js which webpack will bundle

---
+++ index.js
@@ -0,0 +1,9 @@
+require('angular')
+require('angular-animate')
+require('angular-ui-router')
+
+require('./app.js')
+require('./app-services/product.service.js')
+require('./overview/main.controller.js')
+require('./products/main.controller.js')
+require('./products/add-edit.controller.js')
